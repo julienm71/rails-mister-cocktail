1939f7d14e8dc73696157116e9ff33e6867c2faf
Julien Martin <julienm.contact@gmail.com>
2018-05-10 18:41:15 +0200

    css fix error messages

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,5 +4,6 @@ class Dose < ApplicationRecord
   validates_associated :ingredient
   validates_associated :cocktail
   validates :description, presence: true
+  validates :ingredient, presence: true, allow_blank: false
   validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
 end
